64b68fa572029f91d823532b9d8b9e8c0b3c0452
ReisondVale <reisondovale@gmail.com>
2024-12-07 15:49:14 -0300

    Create tests for github client

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,4 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(GithubElixirService.Repo, :manual)
+Mox.defmock(GithubElixirService.MockHttpClient, for: GithubElixirService.HttpClient)
+Application.put_env(:github_elixir_service, :http_client, GithubElixirService.MockHttpClient)
