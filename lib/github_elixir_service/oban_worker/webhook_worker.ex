908588e671e90bb5a8ad4aad4c65d8ea067c95fc
ReisondVale <reisondovale@gmail.com>
2024-12-08 23:19:48 -0300

    Refactor controller and job

--- lib/github_elixir_service/oban_worker/webhook_worker.ex
+++ lib/github_elixir_service/oban_worker/webhook_worker.ex
@@ -1,21 +1,42 @@
 defmodule GithubElixirService.ObanWorker.WebhookWorker do
+  @moduledoc """
+  This worker schedules a job to fetch GitHub repository data (issues and contributors)
+  and sends the processed data to a specified webhook after 24 hours.
+
+  It uses the `GithubElixirService.GithubClient` to collect the data.
+  """
+
   use Oban.Worker, queue: :default, max_attempts: 1
   require Logger
 
   alias GithubElixirService.GithubClient
   alias GithubElixirService.HttpClient
 
-  @spec perform(map()) :: :ok | {:error, any()}
-  def perform(%{"user" => user, "repository" => repo}) do
-    webhook_url = System.get_env("WEBHOOK_URL")
-    headers = [{"Content-Type", "application/json"}]
+  @spec schedule(String.t(), String.t()) :: {:ok, Oban.Job.t()} | {:error, any()}
+  def schedule(user, repo) do
+    snooze_time = Application.get_env(:github_elixir_service, :webhook_snooze_time)
 
     with {:ok, payload} <- GithubClient.get_issues_and_contributors(user, repo),
          body <- Jason.encode!(payload),
-         {:ok, %HTTPoison.Response{status_code: status_code}} when status_code in [200, 201] <-
-           HttpClient.post(webhook_url, body, headers) do
-      :ok
+         {:ok, scheduled_job = %Oban.Job{}} <-
+           Oban.insert(new(%{"body" => body}, schedule_in: snooze_time)) do
+      {:ok, scheduled_job}
     else
+      {:error, reason} ->
+        Logger.error("Failed to schedule job: #{inspect(reason)}")
+        {:error, reason}
+    end
+  end
+
+  @spec perform(Oban.Job.t()) :: :ok | {:error, any()}
+  def perform(%Oban.Job{args: %{"body" => body}}) do
+    webhook_url = Application.get_env(:github_elixir_service, :webhook_url)
+    headers = [{"Content-Type", "application/json"}]
+
+    case HttpClient.post(webhook_url, body, headers) do
+      {:ok, %{status_code: status_code}} when status_code in [200, 201] ->
+        :ok
+
       {:error, reason} ->
         Logger.error("Failed to send to webhook: #{inspect(reason)}")
         {:error, reason}
--- lib/github_elixir_service_web/controllers/webhook_controller.ex
+++ lib/github_elixir_service_web/controllers/webhook_controller.ex
@@ -1,26 +1,31 @@
 defmodule GithubElixirServiceWeb.WebhookController do
   @moduledoc """
   This controller handles requests related to GitHub webhook integration.
-
-  It schedules a job using Oban to fetch data from GitHub (such as issues and contributors)
-
-  The job is scheduled to run 24 hours after the request is received, ensuring asynchronous
-  processing and flexibility in webhook delivery.
   """
 
   use GithubElixirServiceWeb, :controller
 
+  require Logger
   alias GithubElixirService.ObanWorker.WebhookWorker
 
   @spec fetch_data_issues(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def fetch_data_issues(conn, %{"user" => user, "repository" => repo}) do
-    %{
-      "user" => user,
-      "repository" => repo
-    }
-    |> WebhookWorker.new()
-    |> Oban.insert(schedule_in: 24 * 60 * 60)
+    case WebhookWorker.schedule(user, repo) do
+      {:ok, job} ->
+        Logger.info("GitHub data has been scheduled to be sent to the webhook in 24 hours",
+          job: job
+        )
 
-    json(conn, %{message: "Github data issues is being sent to Webhook.site"})
+        conn
+        |> put_status(200)
+        |> json(%{message: "Webhook will be sent in 24 hours"})
+
+      {:error, reason} ->
+        Logger.error("Error to fetch data issues", reason: reason)
+
+        conn
+        |> put_status(500)
+        |> json(%{message: "Error scheduling webhook"})
+    end
   end
 end
